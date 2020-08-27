e7aaabb29785cef7f7999988957397ab60d5bd2c
max3700 <mm.stepanovich@mail.ru>
2020-08-27 10:24:32 +0300

    one

---
+++ data.py
@@ -0,0 +1,108 @@
+from keras import layers
+from keras.models import Model
+import os
+import cv2
+import string
+import numpy as np
+import matplotlib.pyplot as plt
+
+symbols = string.ascii_lowercase + "0123456789"
+num_symbols = len(symbols)
+img_shape = (50, 200, 1)
+
+
+def create_model():
+    img = layers.Input(shape=img_shape)  # Get image as an input and process it through some Convs
+    conv1 = layers.Conv2D(32, (3, 3), padding='same', activation='relu')(img)
+    mp1 = layers.MaxPooling2D(padding='same')(conv1)  # 100x25
+    conv2 = layers.Conv2D(64, (3, 3), padding='same', activation='relu')(mp1)
+    mp2 = layers.MaxPooling2D(padding='same')(conv2)  # 50x13
+    conv3 = layers.Conv2D(64, (3, 3), padding='same', activation='relu')(mp2)
+    bn = layers.BatchNormalization()(conv3)
+    mp3 = layers.MaxPooling2D(padding='same')(bn)  # 25x7
+
+    flat = layers.Flatten()(mp3)
+    outs = []
+    for _ in range(5):
+        dens1 = layers.Dense(64, activation='relu')(flat)
+        drop = layers.Dropout(0.5)(dens1)
+        res = layers.Dense(num_symbols, activation='sigmoid')(drop)
+
+        outs.append(res)
+
+    # Compile model and return it
+    model = Model(img, outs)
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+    return model
+
+
+def preprocess_data():
+    n_samples = len(os.listdir('dataset/'))
+    X = np.zeros((n_samples, 50, 200, 1))  # 1070*50*200
+    y = np.zeros((5, n_samples, num_symbols))  # 5*1070*36
+    for i, pic in enumerate(os.listdir('dataset/')):
+        # Read image as grayscale
+        img = cv2.imread(os.path.join('dataset/', pic), cv2.IMREAD_GRAYSCALE)
+        pic_target = pic[:-4]
+        if len(pic_target) < 6:
+            # Scale and reshape image
+            img = img / 255.0
+            img = np.reshape(img, (50, 200, 1))
+            # Define targets and code them using OneHotEncoding
+            targs = np.zeros((5, num_symbols))
+            for j, l in enumerate(pic_target):
+                ind = symbols.find(l)
+                targs[j, ind] = 1
+            X[i] = img
+            y[:, i] = targs
+    # Return final data
+    return X, y
+
+
+X, y = preprocess_data()
+X_train, y_train = X[:970], y[:, :970]
+X_test, y_test = X[970:], y[:, 970:]
+
+model = create_model()
+model.summary()
+
+hist = model.fit(X_train, [y_train[0], y_train[1], y_train[2], y_train[3], y_train[4]], batch_size=6, epochs=120,
+                 verbose=1, validation_split=0.2)
+
+
+def predict(filepath):
+    img = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+    if img is not None:
+        img = img / 255.0
+    else:
+        print("Not detected")
+    res = np.array(model.predict(img[np.newaxis, :, :, np.newaxis]))
+    ans = np.reshape(res, (5, 36))
+    l_ind = []
+    for a in ans:
+        l_ind.append(np.argmax(a))
+
+    capt = ''
+    for l in l_ind:
+        capt += symbols[l]
+    return capt
+
+
+score = model.evaluate(X_test, [y_test[0], y_test[1], y_test[2], y_test[3], y_test[4]], verbose=1)
+print('Test Loss and accuracy:', score)
+
+model.evaluate(X_test, [y_test[0], y_test[1], y_test[2], y_test[3], y_test[4]])
+
+c = 0
+d = 0
+for i, pic in enumerate(os.listdir('dataset/')):
+    # Read image as grayscale
+    if i > 970:
+        d += 1
+        img = cv2.imread(os.path.join('dataset/', pic), cv2.IMREAD_GRAYSCALE)
+        plt.imshow(img, cmap=plt.get_cmap('gray'))
+        pr = predict(os.path.join('dataset/', pic))
+        pic_target = pic[:-4]
+        if pr == pic_target:
+            c = c + 1
+print((c / d) * 100)
